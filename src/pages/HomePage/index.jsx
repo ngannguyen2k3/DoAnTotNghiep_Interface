60516d95b1b7c83738c1ce7295b3aeeff427e3ba
tantrinh29 <dinhtrinh5678@gmail.com>
2023-11-25 21:41:10 +0700

    [UI] Homepage

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HomePage } from '/pages/HomePage';
+import Layout from '../../components/Layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
