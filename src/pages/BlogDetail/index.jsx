916373c776b426941c57776054098c56da8f6327
NGUYEN TRUONG THINH <113654882+Thinhnt19@users.noreply.github.com>
2023-11-25 21:09:47 +0700

    update loading blog 25/11

--- src/pages/BlogDetail/index.jsx
+++ src/pages/BlogDetail/index.jsx
@@ -7,5 +7,20 @@ import { Link, useParams } from "react-router-dom";
 import Loading from "../../components/Loading";
 
 export default function BlogDetail(){
-    
+    const { data, isLoading, refetch } = useQuery(
+        ["blog"],
+        () => blogService.fetchBlogBySlug(slug),
+        {
+          retry: 3,
+          retryDelay: 1000,
+        }
+      );
+      const { data: titleBlog } = useQuery(
+        ["allBlogs"],
+        () => blogService.fetchAllBlogs(slug),
+        {
+          retry: 3,
+          retryDelay: 1000,
+        }
+      );
 }
